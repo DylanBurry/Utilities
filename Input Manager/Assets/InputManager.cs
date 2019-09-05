4757ac4590a772b51f07587c1f93a60afb7d2d9e
dylanburry <dylanburry@u.boisestate.edu>
2019-09-05 10:40:36 -0600

    Uploading project
    Publishing unity project

---
+++ Input Manager/Assets/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+
+    public Text outText;
+    public InputField textIn;
+    public Button enterButton;
+
+    void Start()
+    {
+        outText.text = "";
+    }
+
+    public void enterPressed()
+    {
+        outText.text = Utilities.ProcessText(textIn.text);
+    }
+
+}
---
+++ Input Manager/Assets/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class Utilities
+{
+    public static string ProcessText(string input)
+    {
+        int numbers = 0;
+        int words = 0;
+        //my code here
+        //determine whetehr user has input a word or a number
+        //return the string 'word' if the user enters a word
+        //return the string 'number if user enters a number
+        string[] splitInput = input.Split(' ');
+        foreach (string stringPart in splitInput)
+        {
+
+            if (float.TryParse(stringPart, out float output)) 
+            {
+                numbers++;
+            }
+            else
+            {
+                words++;
+            }
+        }
+        input = numbers.ToString() + " numbers " + words.ToString() + " words";
+        return input;
+    }
+}
